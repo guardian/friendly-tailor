30fe4256e73e3573a94ba6d08eb40cca5f42faa1
Anne Byrne <anne.byrne@guardian.co.uk>
2016-05-17 12:45:41 +0100

    amended test to initialise nested maps

--- test/com/gu/friendly/tailor/PageViewStorageTest.scala
+++ test/com/gu/friendly/tailor/PageViewStorageTest.scala
@@ -6,6 +6,7 @@ import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType._
 import com.amazonaws.services.dynamodbv2.model.{AttributeValue, PutItemRequest, UpdateItemRequest}
 
 import scala.collection.convert.wrapAsJava._
+import scala.collection.convert.decorateAll._
 
 class PageViewStorageTest extends org.scalatest.FunSpec with org.scalatest.Matchers {
   it("should update a nested map") {
@@ -15,20 +16,26 @@ class PageViewStorageTest extends org.scalatest.FunSpec with org.scalatest.Match
     val entryKey = Map("browserId" -> new AttributeValue().withS("foo"), "userId" -> new AttributeValue().withS("bar"))
 
     def putPageView(tag: String, path: String, time: Long) {
-      val putItemRequest = new PutItemRequest()
-        .withTableName(table)
-        .withItem(entryKey ++ Map("pageViewsByTag" -> new AttributeValue().withM(Map.empty[String, AttributeValue])))
-
-      client.putItem(putItemRequest)
-
-      val updateItemRequest = new UpdateItemRequest()
+      client.updateItem(new UpdateItemRequest()
         .withTableName(table)
         .withKey(entryKey)
-        .withUpdateExpression("SET pageViewsByTag.#tag = :time")
-        .withExpressionAttributeNames(Map("#tag" -> "politics/eu-referendum"))
-        .withExpressionAttributeValues(Map(":time" -> new AttributeValue().withN(time.toString)))
+        .withUpdateExpression("SET pageViewsByTag = if_not_exists(pageViewsByTag,:initialMap)")
+        .withExpressionAttributeValues(Map(":initialMap" -> new AttributeValue().addMEntry(tag, new AttributeValue().addMEntry(path, new AttributeValue().withN(time.toString))))))
 
-      client.updateItem(updateItemRequest)
+      client.updateItem(new UpdateItemRequest()
+          .withTableName(table)
+          .withKey(entryKey)
+          .withUpdateExpression("SET pageViewsByTag.#tag = if_not_exists(pageViewsByTag.#tag, :initialTagMap)")
+          .withExpressionAttributeNames(Map("#tag" -> tag))
+          .withExpressionAttributeValues(Map(":initialTagMap" -> new AttributeValue().addMEntry(path, new AttributeValue().withN(time.toString))))
+      )
+
+      client.updateItem(new UpdateItemRequest()
+        .withTableName(table)
+        .withKey(entryKey)
+        .withUpdateExpression("SET pageViewsByTag.#tag.#path = :time")
+        .withExpressionAttributeNames(Map("#tag" -> tag, "#path" -> path))
+        .withExpressionAttributeValues(Map(":time" -> new AttributeValue().withN(time.toString))))
     }
 
     LocalDynamoDB.usingTable(client)(table)('browserId -> S, 'userId -> S) {
@@ -36,11 +43,11 @@ class PageViewStorageTest extends org.scalatest.FunSpec with org.scalatest.Match
 
       putPageView("politics/eu-referendum", "/politics/2016/may/13/jeremy-corbyn-young-voters-eu-referendum", 1234)
 
-      client.getItem(table, entryKey).getItem().get("pageViewsByTag").getM.size() should be(1)
+      client.getItem(table, entryKey).getItem().get("pageViewsByTag").getM should contain key "politics/eu-referendum"
 
       putPageView("profile/roberto-tyley", "/info/developer-blog/2015/feb/03/prout-is-your-pull-request-out", 5678)
 
-      client.getItem(table, entryKey).getItem().get("pageViewsByTag").getM.size() should be(2)
+      client.getItem(table, entryKey).getItem().get("pageViewsByTag").getM.keySet().asScala should be (Set("politics/eu-referendum","profile/roberto-tyley"))
     }
   }
 }
